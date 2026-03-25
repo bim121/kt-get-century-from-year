adf730e4871a34db8bb36502693dad9d0587095c
Vitaliy Tishik <vitisik54@gmail.com>
2026-03-25 22:08:47 +0200

    ref

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,7 +1,7 @@
 package mate.academy
 
-val YEARS_IN_CENTURY = 100
-val OFFSET = YEARS_IN_CENTURY - 1
+const val YEARS_IN_CENTURY = 100
+const val OFFSET = YEARS_IN_CENTURY - 1
 
 fun getCentury(year: Int): Int {
     return (year + OFFSET) / YEARS_IN_CENTURY
